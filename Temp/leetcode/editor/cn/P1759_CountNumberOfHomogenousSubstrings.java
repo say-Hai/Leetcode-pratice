e587612aa1fb0305939e1e157e0fbd4a1e94c22f
Administrator <peng1527222047@gmail.com>
2022-12-26 16:03:52 +0800

    2022-12-26:中等题，用计数法

---
+++ Temp/leetcode/editor/cn/P1759_CountNumberOfHomogenousSubstrings.java
@@ -0,0 +1,79 @@
+//给你一个字符串 s ，返回 s 中 同构子字符串 的数目。由于答案可能很大，只需返回对 10⁹ + 7 取余 后的结果。 
+//
+// 同构字符串 的定义为：如果一个字符串中的所有字符都相同，那么该字符串就是同构字符串。 
+//
+// 子字符串 是字符串中的一个连续字符序列。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：s = "abbcccaa"
+//输出：13
+//解释：同构子字符串如下所列：
+//"a"   出现 3 次。
+//"aa"  出现 1 次。
+//"b"   出现 2 次。
+//"bb"  出现 1 次。
+//"c"   出现 3 次。
+//"cc"  出现 2 次。
+//"ccc" 出现 1 次。
+//3 + 1 + 2 + 1 + 3 + 2 + 1 = 13 
+//
+// 示例 2： 
+//
+// 输入：s = "xy"
+//输出：2
+//解释：同构子字符串是 "x" 和 "y" 。 
+//
+// 示例 3： 
+//
+// 输入：s = "zzzzz"
+//输出：15
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 10⁵ 
+// s 由小写字符串组成 
+// 
+//
+// Related Topics 数学 字符串 👍 32 👎 0
+
+package leetcode.editor.cn;
+import java.util.*;
+/**
+ * 1759_统计同构子字符串的数目
+ * @author Lemon
+ * @date 2022-12-26 09:58:32
+ */
+class P1759_CountNumberOfHomogenousSubstrings{
+	 public static void main(String[] args) {
+	 	 Solution solution = new P1759_CountNumberOfHomogenousSubstrings().new Solution();
+		  solution.countHomogenous("abb");
+	 }
+	 
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int countHomogenous(String s) {
+		int num = 1;
+		int k = 1;
+		char c = s.charAt(0);
+		for (int i = 1; i < s.length(); i++) {
+			if(c == s.charAt(i)) {
+				k++;
+			} else {
+				c = s.charAt(i);
+				k = 1;
+			}
+			num = (int)(num + k )% 1000000007;
+		}
+		return num;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
